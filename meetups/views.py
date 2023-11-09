382d3fd0e08fcc80e616898c315b00bcd0a5e86e
solaleh81 <solalehalizadeh81.com>
2023-11-09 16:42:02 +0330

    adding queryset to meetup_details view.

--- meetups/views.py
+++ meetups/views.py
@@ -12,15 +12,16 @@ def index(request):
 
 
 def meetup_details(request, meetup_slug):
-    selected_meetup = {
-        "title": "A First Meetup",
-        "description": "This is the first meetup!",
-    }
-    return render(
-        request,
-        "meetups/meetup-details.html",
-        context={
-            "meetup_title": selected_meetup["title"],
-            "meetup_description": selected_meetup["description"],
-        },
-    )
+    try:
+        selected_meetup = Meetup.objects.get(slug=meetup_slug)
+        return render(
+            request,
+            "meetups/meetup-details.html",
+            context={
+                "meetup_found": True,
+                "meetup_title": selected_meetup.title,
+                "meetup_description": selected_meetup.description,
+            },
+        )
+    except Exception as exc:
+        return render(request, 'meetups/meetup-details.html', context={"meetup_found": False})
