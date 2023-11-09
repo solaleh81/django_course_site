5ac2e3b57ee8dcc2280f98f412c40a1d776688f2
solaleh81 <solalehalizadeh81.com>
2023-11-09 10:08:55 +0330

    adding some details to meetups.

--- meetups/views.py
+++ meetups/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render
 from django.http import HttpResponse
 def index(request):
     meetups = [
-        {'title': 'Meetup 1'},
-        {'title':'Meetup 2'},
+        {'title': 'Meetup 1', 'location': 'New york', 'slug': 'a first meetup'},
+        {'title':'Meetup 2', 'location': 'Paris', 'slug': 'a second meetup'},
     ]
     return render(request, 'meetups/index.html', context={'show_meetups': True, 'meetups': meetups})
