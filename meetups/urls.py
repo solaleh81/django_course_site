61a9ee75c82bd82fa80ab660e6e64d3715bcc57c
solaleh81 <solalehalizadeh81.com>
2023-11-09 13:43:22 +0330

    adding meetup_details url.

--- meetups/urls.py
+++ meetups/urls.py
@@ -2,6 +2,8 @@ from . import views
 from django.contrib import admin
 from django.urls import path
 
+
 urlpatterns = [
-    path('meetups/', views.index),
+    path("meetups/", views.index, name="all-meetups"),
+    path("meetups/<slug:meetup_slug>", views.meetup_details, name="meetup-detail"),
 ]
