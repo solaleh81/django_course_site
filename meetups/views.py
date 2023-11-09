59b3e5352875527fb959b02caa36ce0cf217921c
solaleh81 <solalehalizadeh81.com>
2023-11-09 14:50:27 +0330

    adding queryset to index view.

--- meetups/views.py
+++ meetups/views.py
@@ -1,12 +1,9 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-
+from .models import Meetup
 
 def index(request):
-    meetups = [
-        {"title": "Meetup 1", "location": "New york", "slug": "a-first-meetup"},
-        {"title": "Meetup 2", "location": "Paris", "slug": "a-second-meetup"},
-    ]
+    meetups = Meetup.objects.all()
     return render(
         request,
         "meetups/index.html",
