be918afa14e805acab37287a5df5b9da564a55ea
solaleh81 <solalehalizadeh81.com>
2023-11-10 12:01:13 +0330

    adding LOcation MOdel.

--- meetups/models.py
+++ meetups/models.py
@@ -1,10 +1,17 @@
 from django.db import models
 
+class Location(models.Model):
+    name = models.CharField(max_length=200)
+    address = models.CharField(max_length=300)
+
+    def __str__(self):
+        return f"{self.name} ({self.address})"
 class Meetup(models.Model):
     title = models.CharField(max_length=200)
     slug = models.SlugField(unique=True)
     description = models.TextField()
     image = models.ImageField(upload_to='images')
+    location = models.ForeignKey(Location, on_delete=models.CASCADE, related_name="meetups")
 
     def __str__(self):
         return f"{self.title} - {self.slug}"
