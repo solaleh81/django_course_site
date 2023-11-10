726d56767cfa88a4a575d96737981e4dddabd686
solaleh81 <solalehalizadeh81.com>
2023-11-10 21:07:12 +0330

    refactoring this file.

--- meetups/views.py
+++ meetups/views.py
@@ -1,16 +1,15 @@
 from django.shortcuts import render, redirect
-from django.http import HttpResponse
+
 from .models import Meetup, Participant
 from .forms import RegistrationForm
 
+# Create your views here.
+
 def index(request):
     meetups = Meetup.objects.all()
-    return render(
-        request,
-        "meetups/index.html",
-        context={"show_meetups": True, "meetups": meetups},
-    )
-
+    return render(request, 'meetups/index.html', {
+        'meetups': meetups
+    })
 
 
 def meetup_details(request, meetup_slug):
@@ -24,12 +23,11 @@ def meetup_details(request, meetup_slug):
                 user_email = registration_form.cleaned_data['email']
                 participant, _ = Participant.objects.get_or_create(email=user_email)
                 selected_meetup.participants.add(participant)
-                return redirect('confirm-registration')
-
+                return redirect('confirm-registration', meetup_slug=meetup_slug)
 
         return render(request, 'meetups/meetup-details.html', {
                 'meetup_found': True,
-                'selected_meetup': selected_meetup,
+                'meetup': selected_meetup,
                 'form': registration_form
             })
     except Exception as exc:
@@ -40,6 +38,6 @@ def meetup_details(request, meetup_slug):
 
 def confirm_registration(request, meetup_slug):
     meetup = Meetup.objects.get(slug=meetup_slug)
-    return render(request, 'meetups/registration-success.html', context={
-        'organizer_email': meetup.organizer_email,
+    return render(request, 'meetups/registration-success.html', {
+        'organizer_email': meetup.organizer_email
     })
