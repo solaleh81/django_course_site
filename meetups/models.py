b0326001045db0e6b9cc147b41560fcd552ff6e7
solaleh81 <solalehalizadeh81.com>
2023-11-10 12:19:36 +0330

    adding Participant Model with manytomany field.

--- meetups/models.py
+++ meetups/models.py
@@ -6,12 +6,19 @@ class Location(models.Model):
 
     def __str__(self):
         return f"{self.name} ({self.address})"
+
+class Participant(models.Model):
+    email = models.EmailField(unique=True)
+    def __str__(self):
+        return self.email
+
 class Meetup(models.Model):
     title = models.CharField(max_length=200)
     slug = models.SlugField(unique=True)
     description = models.TextField()
     image = models.ImageField(upload_to='images')
     location = models.ForeignKey(Location, on_delete=models.CASCADE, related_name="meetups")
+    participants = models.ManyToManyField(Participant, blank=True, null=True)
 
     def __str__(self):
         return f"{self.title} - {self.slug}"
