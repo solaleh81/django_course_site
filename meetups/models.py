676c7b8452984b33f69e28df0cd138addd1a0b2c
solaleh81 <solalehalizadeh81.com>
2023-11-10 13:43:04 +0330

    adding organizer_email and date fields.

--- meetups/models.py
+++ meetups/models.py
@@ -1,4 +1,6 @@
 from django.db import models
+from persiantools import jdatetime
+
 
 class Location(models.Model):
     name = models.CharField(max_length=200)
@@ -14,11 +16,17 @@ class Participant(models.Model):
 
 class Meetup(models.Model):
     title = models.CharField(max_length=200)
+    organizer_email = models.EmailField()
+    date = models.DateField()
     slug = models.SlugField(unique=True)
     description = models.TextField()
     image = models.ImageField(upload_to='images')
     location = models.ForeignKey(Location, on_delete=models.CASCADE, related_name="meetups")
     participants = models.ManyToManyField(Participant, blank=True, null=True)
+
+    @property
+    def jalali_created_date(self):
+        return jdatetime.JalaliDateTime.to_jalali(self.date).strftime("%Y/%m/%d")
 
     def __str__(self):
         return f"{self.title} - {self.slug}"
