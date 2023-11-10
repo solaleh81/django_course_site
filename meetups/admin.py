4d38a9f7e0dd14ed1dad618bbcf724384aa94aca
solaleh81 <solalehalizadeh81.com>
2023-11-10 11:44:08 +0330

    adding list_filter and prepopulated_fields.

--- meetups/admin.py
+++ meetups/admin.py
@@ -3,6 +3,8 @@ from .models import Meetup
 
 class MeetupAdmin(admin.ModelAdmin):
     list_display = ('title', 'slug')
+    list_filter = ('title', )
+    prepopulated_fields = {"slug": ('title', )}
 
 
 admin.site.register(Meetup, MeetupAdmin)
